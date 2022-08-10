ced3839b11594764405465e0857d7e05897ce83c
Gokulavasan S <95906159+Gokulavasan15@users.noreply.github.com>
2022-08-10 11:10:31 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,8 +3,8 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler

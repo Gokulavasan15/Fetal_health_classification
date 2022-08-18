fe0452765097d2dd7bd39b0f1d876f1973c9873d
Gokulavasan S <95906159+Gokulavasan15@users.noreply.github.com>
2022-08-18 12:23:52 +0530

    Add files via upload

--- test1.py
+++ test1.py
@@ -19,12 +19,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
 from sklearn import metrics
 from sklearn.metrics import roc_curve, auc, roc_auc_score
-import mlflow
+from  mlflow import MlflowClient
 import mlflow.sklearn
 import logging
 import sys
 import warnings
 from urllib.parse import urlparse
+from imblearn.over_sampling import SMOTE
+import psycopg2
+
 
 np.random.seed(0)
 def eval_metrics(actual, pred):
@@ -33,30 +36,36 @@ def eval_metrics(actual, pred):
     f1 = f1_score(y_test, y_pred, average="micro")
     accuracy = accuracy_score(y_test, y_pred)
     return precision,recall,f1,accuracy
-data = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)),"fetal_health.csv"))
+data = pd.read_csv("fetal_health.csv")
 #data.head()
 
 X=data.drop(["fetal_health"],axis=1)
 y=data["fetal_health"]
 
+oversample = SMOTE()
+X_over, y_over = oversample.fit_resample(X, y)
+
 #Set up a standard scaler for the features
 col_names = list(X.columns)
 s_scaler = preprocessing.StandardScaler()
-X_df= s_scaler.fit_transform(X)
+X_df= s_scaler.fit_transform(X_over)
 X_df = pd.DataFrame(X_df, columns=col_names) 
-X_train, X_test, y_train,y_test = train_test_split(X_df,y,test_size=0.3,random_state=42)
+X_train, X_test, y_train,y_test = train_test_split(X_df,y_over,test_size=0.3,random_state=42)
 #X_train, X_test, y_train,y_test = train_test_split(X_df,y,test_size=0.3,random_state=42)
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
     mlflow.set_tracking_uri("http://localhost:8000")
-    classifiers=[LogisticRegression(),RandomForestClassifier(),DecisionTreeClassifier(), SVC()]
+    
+    
+    # ne = 150 
+    # md = 12
     ne = int(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     md = int(sys.argv[2])  if len(sys.argv) > 2 else 0.5
     with mlflow.start_run(run_name="Fetal Health"):
         classifier=RandomForestClassifier(n_estimators=ne,max_depth=md,random_state=42)
-        model_lr=classifier.fit(X_train, y_train)
+        model=classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
         (precision,recall,f1,accuracy)=eval_metrics(y_test, y_pred)
 
@@ -81,6 +90,31 @@ if __name__ == "__main__":
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(model_lr, "model", registered_model_name="Random_forest")
+            mlflow.sklearn.log_model(model, "model", registered_model_name="Random_forest")
         else:
-            mlflow.sklearn.log_model(model_lr, "model")
+            mlflow.sklearn.log_model(model, "model")
+
+
+con = psycopg2.connect(database="mlflow", user='mlflow', password='mlflow', host='10.5.0.7', port= '5432'
+)
+cur = con.cursor()
+for row in cur.execute("select run_uuid, max(value) as max_acc from latest_metrics \
+    where key='accuracy' \
+    group by key,run_uuid \
+    order by max_acc desc \
+    limit 1"):
+    run_id=row[0]
+    accuracy=row[1]
+    
+
+for row1 in cur.execute("select version from model_versions where run_id=(?)",(run_id,)):
+    
+    Version =row1[0]
+
+
+client = MlflowClient()
+client.transition_model_version_stage(
+    name="Random_forest",
+    version= Version,
+    stage="Production"
+)
